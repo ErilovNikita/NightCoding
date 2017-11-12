6e1bf4f6d4083020f74e5ab4926b4a52adff2efc
ErilovNikita <minitwiks@gmail.com>
2017-11-13 03:08:11 +0400

    v0.1

---
+++ js/main-block.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+  $('.block').mouseenter(
+    function(){
+
+      $(".block-active").stop().animate({
+        opacity: "1"
+      }, 300),
+
+      $(".text").stop().animate({
+        opacity: "1",
+        bottom: "-30%"
+      }, 300);
+
+    }),
+
+    $('.block').mouseleave(
+      function(){
+
+        $(".block-active").stop().animate({
+          opacity: "0"
+        }, 300),
+
+        $(".text").stop().animate({
+          opacity: "0",
+          bottom: "-100%"
+        }, 300);
+
+      });
+});
